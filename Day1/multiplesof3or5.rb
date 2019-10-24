5a050352843ed0c8c06b7e920687eb2f51c030a6
Dan <baruahd@tcd.ie>
2019-10-25 01:34:46 +0530

    Refactors problem 1

--- Day1/multiplesof3or5.rb
+++ Day1/multiplesof3or5.rb
@@ -9,18 +9,17 @@ require "test/unit/assertions"
 include Test::Unit::Assertions
 
 def multiplesOf3or5(number)
-  multiples = []
-  (1...number).each do |num|
-    if (num % 3) == 0
-      multiples << num
-    elsif (num % 5) == 0
-      multiples << num
-    else
-      next
+  sum = 0
+  i = 3
+  while(i < number)
+    if( i % 3 == 0 || i % 5 == 0)
+      sum = sum + i
     end
+    i += 1
   end
-  return multiples.inject(0, :+)
+  return sum
 end
+
 
 assert_equal(multiplesOf3or5(10), 23)
 assert_not_equal(multiplesOf3or5(11), 23)
