136610a1254c05bb5090eacae686698132a29b94
Dan <baruahd@tcd.ie>
2019-11-01 18:14:15 +0530

    Added solution of the pythagorean triplet problem

--- Day9/pythagorean_triplet.rb
+++ Day9/pythagorean_triplet.rb
@@ -1,8 +1,29 @@
 =begin
 A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
-a2 + b2 = c2
-For example, 32 + 42 = 9 + 16 = 25 = 52.
-There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc such that a + b + c = n.
+a^2 + b^2 = c^2
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the
+product abc such that a + b + c = n.
 =end
 
 require '../spec_helper'
+
+def pythagorean_triplet(n)
+  sum_of_abc = n
+  a = 1
+  b = a
+
+  a.upto(n) do |ai|
+    b.upto(n) do |bj|
+      c = n - ai - bj
+      if((c > 0) && (ai**2 + bj**2 == c**2)) #&& (ai + bj + c = n))
+          return ai * bj * c
+      end
+    end
+  end
+  return 99
+end
+
+assert_equal(480, pythagorean_triplet(24))
+assert_equal(49920, pythagorean_triplet(120))
+assert_equal(31875000, pythagorean_triplet(1000))
